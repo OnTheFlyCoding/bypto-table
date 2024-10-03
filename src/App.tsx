729a85f874f87204da64d1b2b772c15fdd13aeeb
Giovanni Gonzalez <createdtyler@gmail.com>
2024-10-03 17:40:23 -0400

    baseline css for app layed out in index.css. Input ready with event listener to give a set value from the given input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-
+import { useState } from "react";
 function App() {
+  const [amount, setAmount] = useState('');
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+    <main className="max-w-2xl mx-auto px-4 py-7 ">
+      <h1 className="uppercase text-6xl text-center font-light bg-gradient-to-br from-purple-500 to-sky-400 bg-clip-text text-transparent ">Find Cheapest BTC</h1>
+      <input type="text" 
+      value={amount} 
+      onChange={e => setAmount(e.target.value)} />
+    </main>
   )
 }
 
