4753d6622a76498f00f64a8e6583031abb8ef790
Giovanni Gonzalez <createdtyler@gmail.com>
2024-10-03 18:43:03 -0400

    set default css for the input in tsx file, set to be able to accept future css classNames in app.tsx

--- src/input.tsx
+++ src/input.tsx
@@ -1,19 +1,23 @@
 import { ChangeEventHandler } from "react"
-
+//Types
 type InputProps = {
-    value: string,
-    onChange: ChangeEventHandler<HTMLInputElement>
+    value: string;
+    //onChange expects an event and the event target is HTML
+    onChange: ChangeEventHandler<HTMLInputElement>;
+    className?: string;
 
 }
-
+//Input Controller:
 export default function Input(props:InputProps){
     //Props will be the value given in for input.
     //Short for property of input.
-    return(
-
-        <input  type="text" 
-                value={props.value} 
-                onChange={props.onChange} />
+    return (
+        <input 
+        //default class css but is adaptable to take more in the future
+        className={"border border-teal-400 bg-gray-900 p-2" + props.className}
+        type="text" 
+        value={props.value} 
+        onChange={props.onChange}/>
     
-    )
+    );
 }
