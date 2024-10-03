ba9c446802c4c41afd5b80a23d3cf23dd34aac6c
Giovanni Gonzalez <createdtyler@gmail.com>
2024-10-03 19:17:35 -0400

    Stored Input in a div to center align. Coped framework of input on line 10 to AmountInput to return the expected html

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import Input from "./input";
+import AmountInput from "./AmountInput";
 function App() {
   const [amount, setAmount] = useState('');
 
   return (
     <main className="max-w-2xl mx-auto px-4 py-7 ">
       <h1 className="uppercase text-6xl text-center font-light bg-gradient-to-br from-purple-500 to-sky-400 bg-clip-text text-transparent ">Find Cheapest BTC</h1>
-      <Input 
-      value={amount} 
-      onChange={e => setAmount(e.target.value)} />
+      <div className="flex justify-center mt-8 ">
+        <AmountInput value={amount} onChange={e => setAmount(e.target.value)} />
+      </div>
     </main>  
   )
 }
