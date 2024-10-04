7c3e1f1f0a88187e0f881c954ee5be9cdbf75d4f
Giovanni Gonzalez <createdtyler@gmail.com>
2024-10-03 22:07:25 -0400

    Created ResultRow.tsx Contains nasic styling for results once ammount is entered. no connection between entering amoung and desplaying results.

---
+++ src/ResultRow.tsx
@@ -0,0 +1,12 @@
+export default function ResultRow(){
+    return (
+        <div className="border min-h-6 border-teal-400 
+                        rounded-lg bg-gradient-to-l
+                        from-purple-500/10 to-sky-400/30">
+            <section>
+                result 1
+            </section>
+            
+        </div>
+    )
+}
